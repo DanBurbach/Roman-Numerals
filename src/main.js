342b90b4980d5b31642e3390a871844aa1e769a4
Daniel Burbach and Phil Mass <daniel@>
2019-01-08 09:55:13 -0800

    add karma and jasmine config

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@
 
 //import { Entry } from './entry';
 import './styles.css';
+import $ from 'jquery';
+//import 'boostrap';
+//import 'boostrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function() {
   console.log("ready");
