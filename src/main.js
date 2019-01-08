4666ac1d192cd2f8c10b70c57fb11accd3e7a244
Daniel Burbach and Phil Mass <phil@>
2019-01-08 10:26:10 -0800

    add karma, update class to compile

--- src/main.js
+++ src/main.js
@@ -1,11 +1,23 @@
 'use strict';
 
-//import { Entry } from './entry';
+import { Roman } from './roman';
 import './styles.css';
 import $ from 'jquery';
-//import 'boostrap';
-//import 'boostrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function() {
-  console.log("ready");
+  $("form#romanization").submit(function(event) {
+    event.preventDefault();
+
+    var arabicNumbers = parseInt($("input#numericalInput").val());
+
+    let roman = new Roman();
+    if (arabicNumbers >= 1 && arabicNumbers <= 3999) {
+      var result = roman.romanize(arabicNumbers);
+      $("#result").text(result);
+    } else {
+      $("#result").text("Romans only used numbers from 1 to 3,999. Please enter a number in that range.");
+    }
+  });
 });
